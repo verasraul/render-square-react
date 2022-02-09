e91b299aa0c636adbc8923ac93cbcf484d2fff42
Raul Veras <verasraul@gmail.com>
2022-02-08 23:14:32 -0500

    adding text in square

--- src/components/RenderSqare.jsx
+++ src/components/RenderSqare.jsx
@@ -14,8 +14,18 @@ export default function RenderSqare() {
 
     return (
     <div>
-      <input type={'text'} placeholder='Enter Color' onChange={text => newColor(text.target.value)} /> 
-      <div className="box-display" style={{justifyContent:'center', alignItems: 'center', "height": 100, "width": 100, "backgroundColor": color}}>
+        <br></br>
+      <input type={'text'} placeholder='Enter Color' onChange={text => newColor(text.target.value)} />
+      <br></br>
+      <div className="box-display" style={{
+          "display":'flex',
+          "justifyContent":'center', 
+          "alignItems": 'center', 
+          "height": 200, 
+          "width": 200, 
+          "backgroundColor": color,
+          "color": 'white' }}>
+              <p>Color will display here</p>
 
       </div>
     </div>
